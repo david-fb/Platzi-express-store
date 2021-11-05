8976027417fc90632a1d57cbfdfba14a94e8792f
David Basto <davidbasto01@gmail.com>
2021-11-05 17:29:38 -0500

    final curso, despliegue a heroku

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const routerApi = require('./routes')
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
 const { logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorHandler');
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://127.0.0.1:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin){
+      callback(null, true)
+    }else{
+      callback(new Error('not authorized'))
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res)=>{
   res.send('Hello my express server');
