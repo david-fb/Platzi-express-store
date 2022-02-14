3b6a45808e1786fa2373f3862000aa268a3eb907
David Basto <davidbasto01@gmail.com>
2022-02-13 22:14:00 -0500

    [ADD] ORM sequelize

--- services/productsService.js
+++ services/productsService.js
@@ -1,13 +1,11 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgresPool')
+const sequelize = require('../libs/sequelize')
 class ProductsService {
   constructor(){
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.log(err));
   }
 
   async generate(){
@@ -34,8 +32,8 @@ class ProductsService {
 
   async find(){
     const query = 'SELECT * FROM tasks';
-    const response = await this.pool.query(query);
-    return response.rows;
+    const [data, metadada] = await sequelize.query(query);
+    return {data, metadada};
   }
 
   async findOne(id){
