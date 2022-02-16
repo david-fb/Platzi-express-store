96093b4098003b40ea26990618353aa494a31511
David Basto <davidbasto01@gmail.com>
2022-02-15 22:07:39 -0500

    [UPDATE] change to mysql

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
+const USER = 'root';
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: 'mysql',
   logging: true,
 });
 
