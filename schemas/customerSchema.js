8a153c0b02de4e62654fe64b78d012068879fc8b
David Basto <davidbasto01@gmail.com>
2022-02-21 17:13:00 -0500

    [FIX] typo

--- schemas/customerSchema.js
+++ schemas/customerSchema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const id = Joi.number().interger();
+const id = Joi.number().integer();
 const name = Joi.string().min(3).max(30);
 const lastName = Joi.string();
 const phone = Joi.string();
