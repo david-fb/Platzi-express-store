401103d58516363b75566d73d427abb72e071894
David Basto <davidbasto01@gmail.com>
2022-03-01 15:56:12 -0500

    [FIX] change migration schema

--- db/migrations/20220227020828-order.js
+++ db/migrations/20220227020828-order.js
@@ -1,10 +1,43 @@
 'use strict';
-
-const { OrderSchema, ORDER_TABLE } = require('./../models/orderModel');
+const { DataTypes, Sequelize } = require('sequelize');
+const { ORDER_TABLE } = require('./../models/orderModel');
+const { CUSTOMER_TABLE } = require('./../models/customerModel');
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: true,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      state: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: 'paid',
+        validate: {
+          isIn: [['paid', 'shipped', 'delivered']]
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+      },
+    });
   },
 
   async down (queryInterface) {
