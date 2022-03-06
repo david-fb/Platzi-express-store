4cd46b9bebd3a8bc481104b1c6607a41a8440641
David Basto <davidbasto01@gmail.com>
2022-03-05 22:34:21 -0500

    [ADD] category create endpoint with token

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 
 const CategoryService = require('./../services/categoryService');
 const validatorHandler = require('./../middlewares/validatorHandler');
@@ -29,6 +30,7 @@ router.get('/:id',
   });
 
 router.post('/',
+  passport.authenticate('jwt', {session: false}),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
