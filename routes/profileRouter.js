5184601fccdb2df70ec1ca3154cf2cf2430de8f1
David Basto <davidbasto01@gmail.com>
2022-03-16 16:59:27 -0500

    [ADD] get user profile

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -10,7 +10,25 @@ const service = new OrderService();
 const CustomerService = require('../services/customerService');
 const customerService = new CustomerService();
 
+const UserService = require('../services/userService');
+const userService = new UserService();
+
 const router = express.Router();
+
+router.get('/',
+  passport.authenticate('jwt', { session:false }),
+  async (req, res, next) => {
+    try {
+      const userId = req.user.sub;
+      const userData = await userService.findOne(userId);
+      delete userData.dataValues.password;
+      delete userData.dataValues.recoveryToken;
+      res.json(userData);
+    } catch (error) {
+      next(error)
+    }
+  }
+);
 
 router.get('/my-orders',
   passport.authenticate('jwt', { session:false }),
